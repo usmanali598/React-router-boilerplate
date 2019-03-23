1c917d3e9851f3b5d047246cf480ee641ded07d1
usman <usman_pervaiz@live.com>
2019-03-23 22:45:05 +0200

    style added

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -8,13 +8,16 @@ class Nav extends Component
 {
     render(){
         return (
-            <nav>
-    <div class="nav-wrapper">
-      {/* <a href="#" class="brand-logo">Logo</a> */}
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><NavLink to='/'>Home</NavLink></li>
+            <nav className='nav-wrapper red darken-3'>
+    <div className="nav-wrapper red">
+      <a href="#" className="brand-logo">Router Basics</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href='/'>Home</a></li>
+        <li><a href='/about'>About</a></li>
+        <li><a href='/contact'>Contact</a></li>
+        {/* <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink to='/contact'>Contact</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li> */}
       </ul>
     </div>
   </nav>
